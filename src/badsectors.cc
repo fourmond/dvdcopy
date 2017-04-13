c79c8c8e130a25b7af9d8b2f09e2e0c31c1ad318
Vincent <vincent.fourmond@9online.fr>
2017-04-13 02:42:43 +0200

    Draft of a bad sectors handling class

---
+++ src/badsectors.cc
@@ -0,0 +1,169 @@
+/**
+    \file badsectors.cc
+    Implementation of the B class
+    Copyright 2017 by Vincent Fourmond
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "headers.hh"
+#include "badsectors.hh"
+
+#include "dvdreader.hh"
+
+
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#include <sys/time.h>
+
+// use of regular expressions !
+#include <regex.h>
+
+
+BadSectorsFile::BadSectorsFile(const std::string & file) :
+  fileName(file)
+{
+  readBadSectors();
+}
+
+/// Write out to the bad sector file
+void BadSectorsFile::writeOut()
+{
+  if(fileName.empty())
+    throw std::runtime_error("Can't write unnamed bad sectors file");
+  FILE * out = fopen(fileName.c_str(), "w");
+  if(! out)
+    throw std::runtime_error("Error opening file");
+  for(auto it = badSectors.begin(); it != badSectors.end(); ++it) {
+    const std::string & file = it->first;
+    const std::set<int> & lst = it->second;
+    int first = -1, last = -1;
+    auto it2 = lst.begin();
+    do {                        // relies on the fact sets are ordered
+      bool write = false;
+      int cur = *it2;
+      if(first < 0) {
+        first = cur;
+        last = cur;
+      }
+      else {
+        if(last < cur -1)
+          write = true;
+        else
+          last = cur;
+      }
+      ++it2;
+      if(write || it2 == lst.end()) {
+        fprintf(out, "%s: %d (%d)\n",
+           file.c_str(),
+           first, last-first + 1);
+        first = cur;
+        last = cur;
+      }
+    } while(it2 != lst.end());
+  }
+  fclose(out);
+}
+
+void BadSectorsFile::readBadSectors()
+{
+  FILE * in = fopen(fileName.c_str(), "r");
+  if(! in)
+    return;                     // nothing to read;
+
+  char buffer[1024];
+  regex_t re;
+  regmatch_t matches[6];
+  { 
+    int er = regcomp(&re, "([^:]+): *([0-9]+,[0-9]+,[0-9]+)? *"
+                     "([0-9]+) *\\(([0-9]+)\\)",
+                     REG_EXTENDED);
+    if(er) {
+      regerror(er, &re, buffer, sizeof(buffer));
+      fprintf(stderr, "Error building the line regexp: %s", buffer);
+      return;
+    }
+  }
+    
+  while(! feof(in)) {
+    fgets(buffer, sizeof(buffer), in);
+    int status = regexec(&re, buffer, sizeof(matches)/sizeof(regmatch_t),
+                         matches, 0);
+    if(status) {
+      fprintf(stderr, "error parsing line: %s", buffer);
+    }
+    else {
+      // Make all substrings NULL-terminated:
+      for(int i = 1; i < sizeof(matches)/sizeof(regmatch_t); i++) {
+        if(matches[i].rm_so >= 0)
+          buffer[matches[i].rm_eo] = 0;
+      }
+      
+      // No validation whatsoever, but all groups should be here anyway !
+      std::string file = buffer + matches[1].rm_so;
+      int beg = atoi(buffer + matches[3].rm_so);
+      int size = atoi(buffer + matches[4].rm_so);
+
+      markBadSectors(file, beg, size);
+    }
+  }
+}
+
+void BadSectorsFile::markBadSectors(const std::string & file,
+                                    int pos, int nb)
+{
+  std::set<int> & tgt = badSectors[file];
+  while(nb > 0) {
+    tgt.insert(pos++);
+    --nb;
+  }
+}
+
+void BadSectorsFile::markBadSectors(const DVDFileData * file,
+                                    int pos, int nb)
+{
+  markBadSectors(file->fileName(), pos, nb);
+}
+
+void BadSectorsFile::clearBadSectors(const std::string & file,
+                                     int pos, int nb)
+{
+  std::set<int> & tgt = badSectors[file];
+  while(nb > 0) {
+    tgt.erase(pos++);
+    --nb;
+  }
+}
+
+void BadSectorsFile::clearBadSectors(const DVDFileData * file,
+                                    int pos, int nb)
+{
+  clearBadSectors(file->fileName(), pos, nb);
+}
+
+const std::set<int> & BadSectorsFile::badSectorsForFile(const DVDFileData * file)
+{
+  return badSectors[file->fileName()];
+}
+
+void BadSectorsFile::clear()
+{
+  badSectors.clear();
+}
---
+++ src/badsectors.hh
@@ -0,0 +1,72 @@
+/**
+    \file badsectors.hh
+    Storage and handling of bad sectors
+    Copyright 2017 by Vincent Fourmond
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __BADSECTORS_H
+#define __BADSECTORS_H
+
+class DVDFileData;
+
+/// This class represents the whole set of bad sectors in a DVDs
+class BadSectorsFile {
+protected:
+  
+  /// The list of bad sectors, indexed using the file name returned by
+  /// DVDFileData::fileName()
+  std::map<std::string, std::set<int> > badSectors;
+
+
+  /// The file name
+  std::string fileName;
+
+
+  /// Marks the given sectors as bad sectors
+  void markBadSectors(const std::string & file, int pos, int nb);
+
+  /// Marks the given sectors as good sectors
+  void clearBadSectors(const std::string & file, int pos, int nb);
+
+public:
+
+  /// Constructs a file
+  BadSectorsFile(const std::string & file);
+
+  /// Reads the list of bad sectors from the file
+  void readBadSectors();
+
+  /// Write out to the bad sector file
+  void writeOut();
+
+  /// Marks the given sectors as bad sectors
+  void markBadSectors(const DVDFileData * file, int pos, int nb);
+
+  /// Marks the given sectors as good sectors
+  void clearBadSectors(const DVDFileData * file, int pos, int nb);
+
+  /// Returns the bad sectors for the given file
+  const std::set<int> & badSectorsForFile(const DVDFileData * file);
+
+  /// Clears the bad sectors file
+  void clear();
+
+};
+
+
+
+
+#endif
